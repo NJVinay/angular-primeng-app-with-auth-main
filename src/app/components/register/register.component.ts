f4b7bf4194f9c6b5776ae9002a518e2d967f771e
NJVinay <jv5102003@gmail.com>
2024-07-16 14:24:21 +0530

    Initial commit

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { User } from 'src/app/interfaces/auth';
+import { AuthService } from 'src/app/services/auth.service';
+import { passwordMatchValidator } from 'src/app/shared/password-match.directive';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  registerForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)]],
+    lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)]],
+    gender: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  }, {
+    validators: passwordMatchValidator
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private messageService: MessageService,
+    private router: Router
+  ) { }
+
+  get firstName() {
+    return this.registerForm.controls['firstName'];
+  }
+
+  get lastName() {
+    return this.registerForm.controls['lastName'];
+  }
+
+  get gender() {
+    return this.registerForm.controls['gender'];
+  }
+
+  get phone() {
+    return this.registerForm.controls['phone'];
+  }
+
+  get email() {
+    return this.registerForm.controls['email'];
+  }
+
+  get password() {
+    return this.registerForm.controls['password'];
+  }
+
+  get confirmPassword() {
+    return this.registerForm.controls['confirmPassword'];
+  }
+
+  submitDetails() {
+    const postData = { ...this.registerForm.value };
+    delete postData.confirmPassword;
+    this.authService.registerUser(postData as User).subscribe(
+      response => {
+        console.log(response);
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Register successfully' });
+        this.router.navigate(['login'])
+      },
+      error => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+      }
+    )
+  }
+
+}
